7ee50513c3b2782b791229deb9600452d3098759
Benjamin <psybn3@nottingham.ac.uk>
2022-11-29 21:32:28 +0000

    Adding working example of button event: - A working example of a button command being detected has been added to 'main' of interactiveUI - An additional comment has been included at the top explaining the process of re-creating the resources qrc file.

--- UIPrototype/interactiveUI/main.py
+++ UIPrototype/interactiveUI/main.py
@@ -1,7 +1,11 @@
+#!!NOTE!! - When rebuilding the 'resources_rc.py' file, you need to go into the file and change the import to 'from PySide6 import QtCore' for icons to properly show
+
 import os
 import sys
 
 from ui_interface import *
+
+
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
@@ -9,8 +13,12 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         
+        self.ui.pushButton.clicked.connect(test)
+        
         self.show()
         
+def test():
+    print("test")        
         
 app = QApplication(sys.argv)
 
