9d5014c193895480fafd3a021520728ff6c500fa
psyer2 <psyer2@exmail.nottingham.ac.uk>
2022-11-29 08:48:25 +0000

    Using QTDesigner to make layouts

--- EthanUI/Udemy/Gridlayout.py
+++ EthanUI/Udemy/Gridlayout.py
@@ -1,7 +1,9 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QGridLayout
-from PyQt6.QtGui import QIcon
+from PyQt6.QtWidgets import (QApplication, QWidget, 
+QVBoxLayout, QPushButton, QGridLayout, QHBoxLayout, QLabel)
+from PyQt6.QtGui import QIcon, QFont
 import sys
 
+# Also includes event handling
 
 class Window(QWidget):
     def __init__(self):
@@ -10,19 +12,35 @@ class Window(QWidget):
         self.setGeometry(200,200,700,400)
         self.setWindowTitle("GridLayout Layout")
 
-        grid = QGridLayout()
-        btn1 = QPushButton("btn 1")
-        btn2 = QPushButton("btn 1")
-        btn3 = QPushButton("btn 1")
-        btn4 = QPushButton("btn 1")
+        # grid = QGridLayout()
+        # btn1 = QPushButton("btn 1")
+        # btn2 = QPushButton("btn 1")
+        # btn3 = QPushButton("btn 1")
+        # btn4 = QPushButton("btn 1")
 
-        grid.addWidget(btn1, 0, 0)
-        grid.addWidget(btn2, 0, 1)
-        grid.addWidget(btn3, 1, 0)
-        grid.addWidget(btn4, 0, 0)
+        # grid.addWidget(btn1, 0, 0)
+        # grid.addWidget(btn2, 0, 1)
+        # grid.addWidget(btn3, 1, 0)
+        # grid.addWidget(btn4, 0, 0)
 
-        self.setLayout(grid)
+        # self.setLayout(grid)
 
+        self.create_widget()
+
+    def create_widget(self):
+        hbox = QHBoxLayout()
+        btn = QPushButton("Change Text")
+        btn.clicked.connect(self.clicked_btn)
+        self.label = QLabel("Default Text")
+        hbox.addWidget(btn)
+        hbox.addWidget(self.label)
+        self.setLayout(hbox)
+
+
+    def clicked_btn(self):
+        self.label.setText("Another Text")
+        self.label.setFont(QFont("Times", 15))
+        self.label.setStyleSheet("color: red")
 
 app = QApplication(sys.argv)
 window = Window()
