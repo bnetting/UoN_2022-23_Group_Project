02fe2638e15bfb05ab0fc9fd69f73749200ce5c0
Benjamin Netting <psybn3@nottingham.ac.uk>
2022-12-13 13:47:09 +0000

    Commiting new files and pictures to the repository

--- UIPrototype/prototypeDevelopment1/main.py
+++ UIPrototype/prototypeDevelopment1/main.py
@@ -1,11 +1,13 @@
-#  pyuic6 -x fileName.ui -o {newName.py}
+#  pyuic6 -x welcome_page.ui -o ui_welcome_page.py
+#  pyrcc5 resources.qrc -o resources_rc.py -- Change PyQt5 to PyQt6 in generated file
+
 import os
 import sys # sys variables needed to run UI
 from ui_welcome_page import * # Python version of the UI file for welcome page
 from PyQt6.QtWidgets import QMainWindow, QApplication
+import resources_rc
 
-
-class MainWindow(QMainWindow):
+class MainWindow(QMainWindow): 
     def __init__(self, parent=None): 
         QMainWindow.__init__(self) 
         self.ui = Ui_MainWindow() #puts ui_welcome_Page into variable
