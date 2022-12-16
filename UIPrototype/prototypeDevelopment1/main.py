1598300a15ce8e12368d0c07f2631c644461a6bf
Benjamin Netting <psybn3@nottingham.ac.uk>
2022-12-16 17:52:29 +0000

    New animation code for VCTI label added:
    - Added new animation code for the VCTI header.

--- UIPrototype/prototypeDevelopment1/main.py
+++ UIPrototype/prototypeDevelopment1/main.py
@@ -1,12 +1,12 @@
 #  pyuic6 -x welcome_page.ui -o ui_welcome_page.py
 #  pyrcc5 resources.qrc -o resources_rc.py -- Change PyQt5 to PyQt6 in generated file
 #  Ensure welcome.py is the at the top of the stack before compiling.
-
+import time
 import os
 import sys # sys variables needed to run UI
 from main_user_interface import * # Python version of the UI file for welcome page
-from PyQt6.QtWidgets import QMainWindow, QApplication
-from PyQt6.QtCore import QPropertyAnimation, QEasingCurve
+from PyQt6.QtWidgets import QMainWindow, QApplication, QGraphicsOpacityEffect
+from PyQt6.QtCore import QPropertyAnimation, QEasingCurve, QParallelAnimationGroup
 import resources_rc
 class MainWindow(QMainWindow): #Setup code for welcome page
     def __init__(self, parent=None): 
@@ -26,20 +26,29 @@ class MainWindow(QMainWindow): #Setup code for welcome page
     def firstSetup(self):
 
         self.ui.bottom_tab.setMaximumHeight(0)
-                
+        
         self.ui.animation1 = QPropertyAnimation(self.ui.bottom_tab, b"maximumHeight")
-        self.ui.animation1.setDuration(1000)
+        self.ui.animation1.setDuration(1500)
         self.ui.animation1.setStartValue(0)
         self.ui.animation1.setEndValue(250)
         self.ui.animation1.setEasingCurve(QEasingCurve.Type.InOutQuart)
         self.ui.animation1.start()
-        
+                
         self.ui.animation2 = QPropertyAnimation(self.ui.bottom_tab, b"maximumHeight")
-        self.ui.animation2.setDuration(1000)
+        self.ui.animation2.setDuration(1500)
         self.ui.animation2.setStartValue(0)
         self.ui.animation2.setEndValue(250)
         self.ui.animation2.setEasingCurve(QEasingCurve.Type.InOutQuart)
         self.ui.animation2.start()
+        
+        self.ui.effect1 = QGraphicsOpacityEffect()
+        self.ui.middle_widget.setGraphicsEffect(self.ui.effect1)
+        self.ui.animation3 = QPropertyAnimation(self.ui.effect1, b"opacity")
+        self.ui.animation3.setDuration(3500)
+        self.ui.animation3.setStartValue(0)
+        self.ui.animation3.setEndValue(1)
+        self.ui.animation3.setEasingCurve(QEasingCurve.Type.InOutCubic)
+        self.ui.animation3.start()
         
 app = QApplication(sys.argv)
 
