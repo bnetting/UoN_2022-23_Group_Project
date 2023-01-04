7fa9ba689debeef3c6e65b0447650412a68374b1
Benjamin Netting <psybn3@nottingham.ac.uk>
2023-01-04 16:59:45 +0000

    Add animations to each of the current pages in the ui. comp2002/2022-2023/team31_project#7
    - Animations now added inbetween pages Welcome, Login and Register pages
    - Added another file called graphingExperiments.py that is used to see what graphs we can put in a QT UI

---
+++ UIPrototype/graphingExperiments.py
@@ -0,0 +1,16 @@
+
+import plotly.express as px
+import plotly.graph_objects as go
+import pyqtgraph as pg
+import PySide6
+#-------------------- Show external HTML graph (Fancy stuff)------------------------
+# https://plotly.com/python/
+# figure1 = px.line(x=["a","b","c"], y=[1,3,2], title="sample figure")
+# figure1.show()
+
+# figure2 = go.Figure(data=[go.Bar(y=[2,1,3])],layout_title_text = "Displayed with fig.show()")
+# figure2.show()
+
+
+#-------------------- Show internal graph (Fancy stuff)------------------------
+# https://pyqtgraph.readthedocs.io/en/latest/getting_started/index.html#getting-started-ref
