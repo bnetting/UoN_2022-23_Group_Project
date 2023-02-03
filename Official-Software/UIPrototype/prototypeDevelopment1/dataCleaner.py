9883e21f3bc1b198aacda285fa77336abb2b50f5
Ben Netting <psybn3@nottingham.ac.uk>
2023-02-03 12:51:05 +0000

    Adding a rollback of the dataCleaner.py comp2002/2022-2023/team31_project#20
    - Added a rollback vesion of dataCleaner for demonstrative processes

--- Official-Software/UIPrototype/prototypeDevelopment1/dataCleaner.py
+++ Official-Software/UIPrototype/prototypeDevelopment1/dataCleaner.py
@@ -1,6 +1,5 @@
 """
 Data analysis for threat database
-Search Functionality Included
 Libraries needed "pip install <>"
 - pandas
 - math
@@ -10,11 +9,8 @@ Author - Anderson Jolly
 import math
 import pandas as pd
 
-# TODO add data to excel
 global df
-
-# open excel database
-df = pd.read_excel(r'threats.xlsx')  # read in the data
+df = pd.read_excel(r'UIPrototype\prototypeDevelopment1\threats.xlsx')  # read in the data
 df.pop(df.columns[0])  # remove first column
 
 NETWORK = 0
@@ -38,29 +34,20 @@ Directory_traversal = 10
 Http_response_splitting = 11
 
 
-class Threat:
-    def __init__(self, id, type, category, description, severity, score, exploitability, impact, date, link1, link2):
-        self.cve = id
-        self.type = type
-        self.category = category
-        self.description = description
-        self.severity = severity
-        self.score = score
-        self.exploitability = exploitability
-        self.impact = impact
-        self.date = date
-        self.link1 = link1
-        self.link2 = link2
-
-
 # MAPPING
-def mapData():
-    df['TYPE'] = df['TYPE'].map({'NETWORK': 0, 'LOCAL': 1})
-    df['SEVERITY'] = df['SEVERITY'].map({'LOW': 0, 'MEDIUM': 1, 'HIGH': 2})
-    df['CATEGORY'] = df['CATEGORY'].map(
-        {'Execute Code': 0, 'Overflow': 1, 'Gain privileges': 2, 'Bypass a restriction or similar': 3,
-         'Denial Of Service': 4, 'CSRF ': 5, 'File Inclusion ': 6, 'Cross Site Scripting': 7, 'Sql Injection': 8,
-         'Obtain Information': 9, 'Directory traversal': 10, 'Http response splitting': 11})
+# print(df['CATEGORY'].unique())
+df['TYPE'] = df['TYPE'].map({'NETWORK': 0, 'LOCAL': 1})
+df['SEVERITY'] = df['SEVERITY'].map({'LOW': 0, 'MEDIUM': 1, 'HIGH': 2})
+df['CATEGORY'] = df['CATEGORY'].map(
+    {'Execute Code': 0, 'Overflow': 1, 'Gain privileges': 2, 'Bypass a restriction or similar': 3,
+     'Denial Of Service': 4, 'CSRF ': 5, 'File Inclusion ': 6, 'Cross Site Scripting': 7, 'Sql Injection': 8,
+     'Obtain Information': 9, 'Directory traversal': 10, 'Http response splitting': 11})
+
+# df['DATE'] = pd.cut(x=df['DATE'], bins=[1, 20, 40, 60,80, 100])
+# df['DATE'] = df['DATE'].str.replace('T', '',)
+
+
+# META DATA
 
 
 def getTheatTypes():
@@ -86,55 +73,23 @@ def getAverageMetricFromCat(column: str, category: int):  # Average metric per c
     return mean
 
 
-def getModalMetricFromCat(column: str, category: int):  # Average metric per category
+def getAverageMetricFromCat(column: str, category: int):  # Average metric per category
     temp = df.loc[df['CATEGORY'] == category]
     mean = temp[column].mode()
     return mean
 
 
-def getCountFromCat(category: int):
+def getCountFromCat(category: str):
     temp = df.loc[df['CATEGORY'] == category].count()
     return temp[0]
 
 
-# SEARCHING
+# getAverageMetricFromCol("SCORE")
+# print(getAverageMetricFromCat("SCORE", Overflow))
+# print(getModalFromCol("CATEGORY"))
+# print(getModalFromCol("TYPE"))
+# print(getCountFromCat(Overflow))
 
-def searchByID(id: str):
-    if id in df['ID'].values:
-        row = df[df['ID'] == id]
-        threat = Threat(row['ID'], row['TYPE'], row['CATEGORY'], row['DESCRIPTION'], row['SEVERITY'], row['SCORE'],
-                        row['EXPLOITABILITY'], row['IMPACT'], row['DATE'], row['LINK'], row['OTHER'])
-    else:
-        print("NOT FOUND")
-        return
-    return threat
-
-
-def filterByMetric(metric: str, value):
-    threats = []
-    row = df[df[metric] == value]
-    numRows = row['ID'].count()
-    for i in range(0, numRows):
-        threat = Threat(row['ID'].iloc[i], row['TYPE'].iloc[i], row['CATEGORY'].iloc[i], row['DESCRIPTION'].iloc[i],
-                        row['SEVERITY'].iloc[i], row['SCORE'].iloc[i],
-                        row['EXPLOITABILITY'].iloc[i], row['IMPACT'].iloc[i], row['DATE'].iloc[i], row['LINK'].iloc[i],
-                        row['OTHER'].iloc[i])
-        threats.append(threat)
-
-    return threats
-
-
-def searchByDesc(text: str):
-    threats = []
-    row = df[df['DESCRIPTION'].str.contains(text)]
-    numRows = row['ID'].count()
-    for i in range(0, numRows):
-        threat = Threat(row['ID'].iloc[i], row['TYPE'].iloc[i], row['CATEGORY'].iloc[i], row['DESCRIPTION'].iloc[i],
-                        row['SEVERITY'].iloc[i], row['SCORE'].iloc[i],
-                        row['EXPLOITABILITY'].iloc[i], row['IMPACT'].iloc[i], row['DATE'].iloc[i], row['LINK'].iloc[i],
-                        row['OTHER'].iloc[i])
-        threats.append(threat)
-
-    return threats
+# getTheatTypes()
 
 
