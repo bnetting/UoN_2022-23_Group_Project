4a5c22e34b29c4c6a498db9b4180c3d002c0ae87
Anderson Jolly <psyaj3@nottingham.ac.uk>
2023-02-04 18:38:11 +0000

    Add: Login Database - add Database class to handle sql statements - table creation for username and password
    comp2002/2022-2023/team31_project#24

---
+++ Login_System/LoginSystem.py
@@ -0,0 +1,65 @@
+"""
+Username and Password Database
+Libraries needed "pip install <>"
+- sql
+- hashlib
+
+Author - Anderson Jolly
+"""
+import sqlite3
+import hashlib
+
+DATABASE_NAME = "userdata.db"
+
+
+class Database:
+    def __init__(self):
+        self.connection = sqlite3.connect(DATABASE_NAME)
+        self.cursor = self.connection.cursor()
+        self.cursor.execute("""
+CREATE TABLE IF NOT EXISTS userdata (
+id INTEGER PRIMARY KEY,
+username VARCHAR(255) NOT NULL,
+password VARCHAR(255) NOT NULL,
+email VARCHAR(255) NOT NULL)
+""")
+
+    def establishConnection(self):
+        return self.connection, self.cursor
+
+    def createUser(self, email: str, username: str, password: str):
+        password = hashlib.sha256(password.encode()).hexdigest()
+        self.cursor.execute("INSERT INTO userdata (username, password, email) VALUES (?, ?, ?)",
+                            (username, password, email))
+        self.connection.commit()
+
+    def getAll(self):
+        return self.cursor.execute("""SELECT * FROM userdata""").fetchall()
+
+    def removeUser(self, id: int):
+        self.cursor.execute("DELETE FROM userdata WHERE id= '%d'" % id)
+        self.connection.commit()
+
+    def deleteAll(self):
+        self.cursor.execute("""DELETE FROM userdata""")
+        self.connection.commit()
+
+    def authenticateUser(self, username, password):
+        password = hashlib.sha256(password).hexdigest()
+        #print(username, password)
+        message = "SELECT * FROM userdata WHERE username = '"+username+"' AND password = '"+password+"'"
+        #print(message)
+        self.cursor.execute(message)
+        self.connection.commit()
+        result = self.cursor.fetchall()
+        if result:
+            print(result[0])
+            return True
+        else:
+            print("fucked")
+            return False
+
+db = Database()
+#db.createUser("andersonjolly@gmail.com", "psyaj3", "password")
+db.authenticateUser("psyaj3", "password".encode())
+print(db.getAll())
